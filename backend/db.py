a59b97b2c7127f30056fb73db9fb3cd96d003e5a
saurabhje <work.saurabh@outlook.com>
2025-07-08 22:56:37 +0530

    Add: localhost support for the database

--- backend/db.py
+++ backend/db.py
@@ -4,4 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_db_connection():
-        return psycopg2.connect(os.getenv("DATABASE_URL"))
+        return psycopg2.connect(host=os.getenv("DB_HOST"),
+                                database=os.getenv("DB_NAME"),
+                                user=os.getenv("DB_USER"),
+                                password=os.getenv("DB_PASSWORD"),
+                                port=os.getenv("DB_PORT", 5432))
+
