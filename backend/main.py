117a4487d36f1db6ab1efabd7909f35756a8515c
saurabhje <work.saurabh@outlook.com>
2025-07-09 10:20:33 +0530

    Add: cors

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from routes import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "https://dashboard.saurabh.codes"
+]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/api")
 
